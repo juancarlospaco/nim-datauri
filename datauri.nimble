60adf1b636f44337ff7167252030a1e899e76dfb
Juan Carlos <juan@thirstie.com>
2018-05-15 15:17:58 -0300

    init.

---
+++ datauri.nimble
@@ -0,0 +1,11 @@
+# Package
+
+version       = "0.1.0"
+author        = "Juan Carlos"
+description   = "Data URI Base64 UTF-8 Nim module"
+license       = "LGPLv3"
+srcDir        = "src"
+
+# Dependencies
+
+requires "nim >= 0.18.0"
---
+++ src/datauri.nim
@@ -0,0 +1,18 @@
+#!/usr/bin/nim c -r
+
+
+import strformat, strutils, mimetypes, base64
+
+
+proc file2datauri*(filename: string): string =
+  fmt"data:{newMimetypes().getMimetype(filename.split('.')[1])};base64,{encode(readFile(filename))}"
+
+
+proc datauri2file*(data_uri: string, filename: string): cint =
+  writeFile(filename, decode(data_uri.split(",")[1]))
+
+
+if is_main_module:
+  # echo file2datauri("probando.txt")
+  echo datauri2file("data:text/plain;charset=utf-8;base64,YQpiCmMKZAplCmYK", "output0.txt")
+  echo datauri2file("data:text/plain;base64,YQpiCmMKZAplCmYK", "output1.txt")
