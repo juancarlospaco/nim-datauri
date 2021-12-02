616b0109df119ac3f60c380d8b5a4fbda3c707c7
Juan Carlos <juancarlospaco@gmail.com>
2021-12-02 15:24:27 -0800

    Update

--- src/datauri.nim
+++ src/datauri.nim
@@ -1,18 +1,34 @@
-#!/usr/bin/nim c -r
+import std/[strutils, mimetypes, base64]
 
-
-import strformat, strutils, mimetypes, base64, algorithm
-
+template file2datauriImpl(filename, mime: string) =
+  result = "data:"
+  result.add mime
+  result.add ';'
+  result.add 'b'
+  result.add 'a'
+  result.add 's'
+  result.add 'e'
+  result.add '6'
+  result.add '4'
+  result.add ','
+  result.add encode(readFile(filename))
 
 proc file2datauri*(filename: string): string =
-  fmt"data:{newMimetypes().getMimetype(filename.split('.').reversed()[0])};base64,{encode(readFile(filename))}"
+  assert filename.len > 0, "filename must not be empty string"
+  let db: MimeDB = newMimetypes()
+  file2datauriImpl(filename, db.getMimetype(filename.split('.')[^1]))
+
+proc file2datauri*(filename, mime: string): string =
+  assert filename.len > 0, "filename must not be empty string"
+  file2datauriImpl(filename, mime)
+
+template datauri2file*(dataUri, filename: string) =
+  assert dataUri.len > 0, "dataUri must not be empty string"
+  assert filename.len > 0, "filename must not be empty string"
+  writeFile(filename, decode(dataUri.split(',', 1)[1]))
 
 
-proc datauri2file*(data_uri: string, filename: string): cint =
-  writeFile(filename, decode(data_uri.split(",")[1]))
-
-
-if is_main_module:
-  # echo file2datauri("probando.txt")
-  echo datauri2file("data:text/plain;charset=utf-8;base64,YQpiCmMKZAplCmYK", "output0.txt")
-  echo datauri2file("data:text/plain;base64,YQpiCmMKZAplCmYK", "output1.txt")
+when isMainModule:
+  echo file2datauri("datauri.nim")
+  datauri2file("data:text/plain;charset=utf-8;base64,YQpiCmMKZAplCmYK", "output0.txt")
+  datauri2file("data:text/plain;base64,YQpiCmMKZAplCmYK", "output1.txt")
